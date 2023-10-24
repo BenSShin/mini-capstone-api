35447e6bb87a038251582f3fd74f0c363748b34f
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-10-24 12:34:36 -0500

    added gem tables to http

--- http.rb
+++ http.rb
@@ -1,7 +1,15 @@
 require "http"
+require "tty-table"
 
-response = HTTP.get("http://localhost:3000/second_product.json")
+response = HTTP.get("http://localhost:3000/products_path.json")
 data = response.parse
 
-p "Here is the second product."
-p data
+header = ["Name", "Price"]
+rows = [[data[0]["name"], data[0]["price"]], [data[1]["name"], data[1]["price"]], [data[2]["name"], data[2]["price"]]]
+
+table = TTY::Table.new(["Name", "Price"], [[data[0]["name"], data[0]["price"]], [data[1]["name"], data[1]["price"]], [data[2]["name"], data[2]["price"]]])
+
+table.render do |renderer|
+  renderer.border.separator = :each_row
+end
+puts table.render(:ascii)
