3cc0384e992ae45d9d3e93775ab6c391c32f395b
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-10-25 12:40:45 -0500

    switched method names to index and show in constroller

--- http_test.rb
+++ http_test.rb
@@ -1,1 +1,23 @@
 require "http"
+
+def show_test
+  response = HTTP.get("http://localhost:3000/products.json")
+  show = response.parse
+  products = Product.all
+  if show.length == products.length
+    pp "Pass!"
+  else
+    pp "Fail!"
+  end
+end
+
+def index_test
+  response = HTTP.get("http://localhost:3000/products/1.json")
+  index = response.parse
+  product = Product.first
+  product_keys = []
+  product.map { |k, v| product_keys << k }
+  pp product_keys
+end
+
+# code does not work since can't pull data from Product class since its in a file which i dont knwo
