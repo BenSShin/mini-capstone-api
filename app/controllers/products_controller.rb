539df8ac93fdc874b28a2e3cf60978595e142cef
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-10-25 13:03:59 -0500

    added create method and route and also exception in protection against forgery

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @product = Product.create(
+      name: "macaroons",
+      price: 4,
+      image_url: "http://google.com",
+      description: "Delicaate chewy shells with caramel buttercream filling",
+    )
+    render :show
+  end
 end
