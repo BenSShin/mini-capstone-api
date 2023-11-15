bd223c34f8c539d7956fc47e82da8701f3447a74
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-15 13:52:16 -0600

    added authotization for index and show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,28 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.where(user_id: current_user.id)
     render :index
   end
 
   def show
     @order = Order.find_by(id: params["id"])
-    render :show
+    if current_user.id && current_user.id == @order.user_id
+      render :show
+    else
+      render json: { message: "You must be logged in to view your orders" }
+    end
   end
 
   def create
     @order = Order.create(
-      user_id: params["user_id"],
+      user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      subtotal: params["subtotal"],
-      tax: params["tax"],
-      total: params["total"],
+    )
+    @order.update(
+      subtotal: @order.subtotal,
+      tax: @order.tax,
+      total: @order.total,
     )
     render :show
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,20 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  def subtotal
+    @product = Product.find_by(id: product_id)
+    subtotal = @product[:price] * quantity
+    return subtotal
+  end
+
+  def tax
+    tax = subtotal * 0.09
+    return tax
+  end
+
+  def total
+    total = subtotal + tax
+    return total
+  end
 end
