14dd033ec350c61472390e05991483fb42a5cc70
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-27 12:37:17 -0600

    added associactions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  belongs_to :supplier
+  has_many :images
+  has_many :orders
+  has_many :category_products
+
+  has_many :categories, through: :category_products
+
   def is_discounted?
     if price <= 10
       return true
@@ -16,16 +23,6 @@ class Product < ApplicationRecord
     total = price + tax
     return total.round(2)
   end
-
-  belongs_to :supplier
-  # def supplier
-  #    Supplier.find_by(id: supplier_id)
-  # end
-  has_many :images
-  # def image
-  #   Image.where(product_id: id)
-  # end
-  has_many :orders
 
   # validates :name, presence: true
   # validates :price, presence: true
