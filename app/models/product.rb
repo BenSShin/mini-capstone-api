e3c38525fdb2f6da1a208b2713845cf7c8b11c56
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-29 12:18:37 -0600

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :carted_products
+  has_many :users, through: :carted_products
 
   has_many :categories, through: :category_products
 
